a8415f327533ca708e1d76c41017fdbbf158cc7e
JessicaBane <jessicambane@gmail.com>
2023-06-28 15:06:40 +0100

    added local storage

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const maxClicksAllowed = 5 // change this to 25 later
 
 let allProjects = [];
 let pastProjects = [];
-let currentVotes = [];
 
 
 function getRandomNumber(){
@@ -28,8 +27,6 @@ function Project(name, src){
     this.clicks = 0;
     this.views = 0;
     allProjects.push(this);
-    saveAllProducts();
-    // console.log(allProjects)
 }
 
 function renderProjects(){
@@ -67,7 +64,6 @@ function renderProjects(){
     console.log(pastProjects)
 
     console.log(`${allProjects[project1].views} while loop is working?`)
-    // console.log(pastProjects)
     savePrevProducts()
 }
 
@@ -88,42 +84,15 @@ function projectClickedOn(event){
     if (clicks === maxClicksAllowed){
         projectContainer.removeEventListener("click", projectClickedOn);
         projectContainer.className = "no-voting";
-        // resultsButton.addEventListener("click", renderResults);
         resultsButton.addEventListener("click", renderChat);
         resultsButton.className = "clicks-allowed"
         image1.classList.add("noProjects")
         image2.classList.add("noProjects")
         image3.classList.add("noProjects")  
-        // saveVotes()    
     } else {
     renderProjects();
     }
 }
-
-const bag = new Project("bag", "images/bag.jpg")
-const banana = new Project("banana","images/banana.jpg")
-const bathroom = new Project("bathroom", "images/bathroom.jpg")
-const boots = new Project("boots","images/boots.jpg")
-const breakfast = new Project("breakfast", "images/breakfast.jpg")
-const bubblegum = new Project("bubblegum","images/bubblegum.jpg")
-const chair = new Project("chair","images/chair.jpg")
-const cthulhu = new Project("cthulhu", "images/cthulhu.jpg")
-const dogduck = new Project ("dog-duck","images/dog-duck.jpg")
-const dragon = new Project("dragon","images/dragon.jpg")
-const pen = new Project("pen","images/pen.jpg")
-const petsweep = new Project("pet-sweep","images/pet-sweep.jpg")
-const scissors = new Project("scissors","images/scissors.jpg")
-const shark = new Project("shark","images/shark.jpg")
-const sweep = new Project("sweep","images/sweep.png")
-const tauntaun = new Project("tauntaun","images/tauntaun.jpg")
-const unicorn = new Project("unicorn", "images/unicorn.jpg")
-const watercan = new Project("water-can","images/water-can.jpg")
-const wineglass = new Project("wine-glass","images/wine-glass.jpg")
-
-// Use this later
-// for (let i = 0; i < productNames.length; i++) {
-//     new Project(productNames[i], `imgs/${productNames[i]}.jpg`);
-// }
 
 function renderResults(){
     console.log("Here are the results")
@@ -134,8 +103,6 @@ function renderResults(){
         ul.appendChild(li);
     }
 }
-
-renderProjects()
 
 projectContainer.addEventListener("click", projectClickedOn)
 
@@ -150,9 +117,8 @@ function renderChat(){
         projectNames.push(allProjects[i].name)
         projectViews.push(allProjects[i].views)
         projectClicks.push(allProjects[i].clicks)
-        currentVotes.push(allProjects[i].clicks)
     }
-    saveVotes()
+
     const data = {
         labels: projectNames,
         datasets: [
@@ -208,6 +174,7 @@ function renderChat(){
 
     const resultChart = new Chart(projectChart,config)
     const resultChart2 = new Chart(projectChart2,config2)
+    saveAllProducts()
 }
 
 
@@ -220,64 +187,49 @@ function saveAllProducts(){
 function savePrevProducts(){
     let previousProducts = JSON.stringify(pastProjects)
     localStorage.setItem("prevProjects", previousProducts)
-    // console.log("This is local stored as strings" + allProductsArray + previousProducts)
-}
-
-function saveVotes(){
-    let prevVotes = JSON.stringify(currentVotes)
-    localStorage.setItem("currentVotes", prevVotes)
-    console.log(prevVotes + "HERE")
 }
 
 function pageLoad(){
     let storedAllProjects = JSON.parse(localStorage.getItem("allProducts"))
     let storedPrevProjects = JSON.parse(localStorage.getItem("prevProjects"))
-    let storedPrevVotes = JSON.parse(localStorage.getItem("currentVotes"))
     console.log("This is locally stored " + storedAllProjects + " and this " + storedPrevProjects)
-    console.log("prev results " + storedPrevVotes)
 
-    if (storedAllProjects === null){
-        console.log("null")
-        // return
+
+    if (storedAllProjects){
+        allProjects = storedAllProjects
     } else {
-        console.log("there is data stored for all projects!!!")
+        console.log("no data here")
+        const bag = new Project("bag", "images/bag.jpg")
+        const banana = new Project("banana","images/banana.jpg")
+        const bathroom = new Project("bathroom", "images/bathroom.jpg")
+        const boots = new Project("boots","images/boots.jpg")
+        const breakfast = new Project("breakfast", "images/breakfast.jpg")
+        const bubblegum = new Project("bubblegum","images/bubblegum.jpg")
+        const chair = new Project("chair","images/chair.jpg")
+        const cthulhu = new Project("cthulhu", "images/cthulhu.jpg")
+        const dogduck = new Project ("dog-duck","images/dog-duck.jpg")
+        const dragon = new Project("dragon","images/dragon.jpg")
+        const pen = new Project("pen","images/pen.jpg")
+        const petsweep = new Project("pet-sweep","images/pet-sweep.jpg")
+        const scissors = new Project("scissors","images/scissors.jpg")
+        const shark = new Project("shark","images/shark.jpg")
+        const sweep = new Project("sweep","images/sweep.png")
+        const tauntaun = new Project("tauntaun","images/tauntaun.jpg")
+        const unicorn = new Project("unicorn", "images/unicorn.jpg")
+        const watercan = new Project("water-can","images/water-can.jpg")
+        const wineglass = new Project("wine-glass","images/wine-glass.jpg")
 
     }    
     
-    if (storedPrevProjects.length === 0 || null){
-        console.log(typeof(storedPrevProjects))
-        console.log("null again")
-        // return
-    } else {
-        console.log("there is data stored for prev projects!!!")
-        console.log(storedPrevProjects)
-
-    }
-
-    if (storedPrevVotes === null){
-        console.log("No votes yet")
-    } else {
-        // console.log("these are the current votes" + storedPrevVotes)
-        for (let i = 0; i < storedPrevVotes.length; i++){
-
-            // newNum = storedPrevVotes
-        }
-        
-        // currentVotes = storedPrevVotes
-        console.log("these are the current votes" + storedPrevVotes)
-
-        // for(let i = 0; i < storedPrevVotes.length; i++){
-        //     currentVotes[i] = currentVotes[i] + storedPrevVotes[i].
-        //     console.log(currentVotes)
-        // }
+    if (storedPrevProjects){
+        pastProjects = storedPrevProjects
     }
 }
 
-// saveAllProducts()
 pageLoad()
+renderProjects()
 
 // localStorage.removeItem("allProducts")
 // localStorage.removeItem("prevProjects")
-// localStorage.removeItem("currentVotes")
 
 
